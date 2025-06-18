169bbc5d83cfdd39cccecaaaa2aab13a742103c9
WZNT-KimHyeJin <pcrs5278@naver.com>
2025-06-18 09:49:04 +0900

    fix : TokenLoader for testing

--- src/main/java/io/goorm/rout33/route33auth/service/auth/TokenPropertyLoader.java
+++ src/main/java/io/goorm/rout33/route33auth/service/auth/TokenPropertyLoader.java
@@ -1,44 +1,16 @@
 package io.goorm.rout33.route33auth.service.auth;
 
-import io.goorm.rout33.route33auth.service.auth.TokenProperty;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 
-import java.io.StringReader;
-import java.util.Properties;
 
 @Component
 public class TokenPropertyLoader {
 
-    private static final String SECRET_NAME = "spring/backend/token-config";
-    private final SecretsManagerClient client;
-
-    public TokenPropertyLoader() {
-        this.client = SecretsManagerClient.builder()
-                .region(Region.of("ap-northeast-2"))
-                .build();
-    }
-
     public TokenProperty load() {
-//        SecretsManagerClient client = SecretsManagerClient.builder()
-//                .region(Region.of("ap-northeast-2"))
-//                .build();
-
-        String secretString = client.getSecretValue(GetSecretValueRequest.builder()
-                        .secretId(SECRET_NAME)
-                        .build())
-                .secretString();
-        System.out.println(secretString);
-
         try {
-            Properties props = new Properties();
-            props.load(new StringReader(secretString));
-
-            String secretKey = props.getProperty("token.secret-key");
-            long access = Long.parseLong(props.getProperty("token.access-token-expiration-millis"));
-            long refresh = Long.parseLong(props.getProperty("token.refreshTokenExpirationMillis"));
+            String secretKey = "cm91dGUzMy10b2tlbi1yb3V0ZTMzLXRva2VuLXJvdXRlMzM=";
+            long access = 3600000L;
+            long refresh = 1209600000L;
 
             return new TokenProperty(secretKey, access, refresh);
 
